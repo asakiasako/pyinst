fdae3325eb623e1e36f11cb89a068ebfb3736ba2
chongjun.lei <chongjun.lei@neophotonics.com>
2018-01-26 17:57:32 +0800

    Finish & Tested TypeOMA and ModelN4392A

--- ins_types.py
+++ ins_types.py
@@ -779,3 +779,41 @@ class TypeOSA(TypeIns):
         :return: (dict) a dict of test_item=>value
         """
         self._raise_no_rewrite()
+
+
+class TypeOMA(TypeIns):
+    def __init__(self, *args, **kwargs):
+        super(TypeOMA, self).__init__()
+        self._append_ins_type(InstrumentType.OMA)
+
+    def get_trace_items(self, trace):
+        """
+        Get all the test item names for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of str) trace item names.
+        """
+        self._raise_no_rewrite()
+
+    def get_trace_values(self, trace):
+        """
+        Get all the test values for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of float) trace item values.
+        """
+        self._raise_no_rewrite()
+
+    def get_trace_units(self, trace):
+        """
+        Get all the units for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of str) units of item values.
+        """
+        self._raise_no_rewrite()
+
+    def get_formatted_data(self, trace):
+        """
+        Get a formatted data include test items, values, and units.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (dict) { str:item1: (float:value, str:unit), ...}
+        """
+        self._raise_no_rewrite()
--- instruments.py
+++ instruments.py
@@ -1341,3 +1341,110 @@ class ModelAQ6370(VisaInstrument, TypeOSA):
                     "lvl_diff_min": data_list[8]
                 }
         return r_data
+
+
+class ModelN4392A(VisaInstrument, TypeOMA):
+    def __init__(self, resource_name, read_termination=READ_TERMINATION, timeout=TIMEOUT):
+        super(ModelN4392A, self).__init__(resource_name, read_termination=read_termination, timeout=timeout)
+        self.__model = "N4392A"
+        self.__brand = "Keysight"
+        self._trace = None
+        self._items = []
+        self._units = []
+
+    # param encapsulation
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, value):
+        raise AttributeError('param model is read-only')
+
+    @property
+    def brand(self):
+        return self.__brand
+
+    @brand.setter
+    def brand(self, value):
+        raise AttributeError('param brand is read-only')
+
+    # Methods
+    def run(self):
+        """
+        Run OMA
+        """
+        self.command(':INIT:RES')
+        
+    def pause(self):
+        """
+        Pause OMA
+        """
+        self.command(':INIT:ABOR')
+    
+    def get_trace_items(self, trace):
+        """
+        Get all the test item names for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of str) trace item names.
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        item_str = self.query(':TRACe%d:DATA:TABLe:NAME?' % trace)
+        item_list = item_str.split(',')
+        item_list = list(map(lambda x: x.strip('"'), item_list))
+        return item_list
+
+    def get_trace_values(self, trace):
+        """
+        Get all the test values for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of float) trace item values.
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        value_str = self.query(':TRACe%d:DATA:TABLe?' % trace)
+        value_list = value_str.split(',')
+        value_list = list(map(lambda x: float(x), value_list))
+        return value_list
+
+    def get_trace_units(self, trace):
+        """
+        Get all the units for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of str) units of item values.
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        unit_str = self.query(':TRACe%d:DATA:TABLe:UNIT?' % trace)
+        unit_list = unit_str.split(',')        
+        unit_list = list(map(lambda x: x.strip('"'), unit_list))
+        return unit_list
+
+    def set_current_trace(self, trace):
+        """
+        Set current trace
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        self._trace = trace
+        self._items = self.get_trace_items(trace)
+        self._units = self.get_trace_units(trace)
+
+    def get_formatted_data(self, trace):
+        """
+        Get a formatted data include test items, values, and units.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (dict) { str:item1: (float:value, str:unit), ...}
+        """
+        if trace != self._trace:
+            self.set_current_trace(trace)
+            return self.get_formatted_data(trace)
+        else:
+            values = self.get_trace_values(trace)
+            rdata = {}
+            for i in range(len(self._items)):
+                rdata[self._items[i]] = (values[i], self._units[i])
+            return rdata
+
