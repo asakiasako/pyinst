4b67e904adb28ecba296de0015a5310e96216139
chongjun.lei <chongjun.lei@neophotonics.com>
2019-08-05 10:58:22 +0800

    mod readme, add information

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .models import *
 from .instrument_types import *
 from .constants import *
 from .functions import *
+from .pkg_info import *
---
+++ pkg_info.py
@@ -0,0 +1,7 @@
+AUTHOR = 'Chongjun Lei'
+
+EMAIL = 'chongjun.lei@neophotonics.com'
+
+COMPANY = 'NeoPhotonics Corporation'
+
+VERSION = '0.8.0'
