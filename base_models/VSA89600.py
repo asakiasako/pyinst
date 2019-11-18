b1d81f6190951143d542b6dd628e6678f3f8543d
chongjun.lei <chongjun.lei@neophotonics.com>
2019-11-18 20:33:46 +0800

    add oma instruments

---
+++ base_models/VSA89600.py
@@ -0,0 +1,95 @@
+from ..base_models._VisaInstrument import VisaInstrument
+from ..utils import check_range, check_type
+
+
+class ModelVSA89600(VisaInstrument):
+
+    def __init__(self, resource_name, **kwargs):
+        super(ModelVSA89600, self).__init__(resource_name, **kwargs)
+        self._trace = None
+        self._item_names = []
+        self._units = []
+
+    # param encapsulation
+
+    # Methods
+    def run(self):
+        """
+        Run OMA
+        """
+        self.command(':INIT:RES')
+        
+    def stop(self):
+        """
+        Pause OMA
+        """
+        self.command(':INIT:ABOR')
+    
+    def get_trace_item_names(self, trace):
+        """
+        Get all the test item names for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of str) trace item names.
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        item_str = self.query(':TRACe%d:DATA:TABLe:NAME?' % trace)
+        item_list = item_str.split(',')
+        item_list = list(map(lambda x: x.strip('"'), item_list))
+        return item_list
+
+    def get_trace_values(self, trace):
+        """
+        Get all the test values for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of float) trace item values.
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        value_str = self.query(':TRACe%d:DATA:TABLe?' % trace)
+        value_list = value_str.split(',')
+        value_list = list(map(lambda x: float(x), value_list))
+        return value_list
+
+    def get_trace_units(self, trace):
+        """
+        Get all the units for the specified trace.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (list of str) units of item values.
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        unit_str = self.query(':TRACe%d:DATA:TABLe:UNIT?' % trace)
+        unit_list = unit_str.split(',')        
+        unit_list = list(map(lambda x: x.strip('"'), unit_list))
+        return unit_list
+
+    def set_active_trace(self, trace):
+        """
+        Set current trace
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        """
+        check_type(trace, int, 'trace')
+        check_range(trace, 1, float('inf'))
+        self._trace = trace
+        self._item_names = self.get_trace_item_names(trace)
+        self._units = self.get_trace_units(trace)
+
+    def get_formatted_trace_data(self, trace):
+        """
+        Get a formatted data include test item_names, values, and units.
+        :param trace: (int) index of trace, 1 based from A. For example: A->1, E->5
+        :return: (dict) { str:item1: (float:value, str:unit), ...}
+        """
+        if trace != self._trace:
+            self.set_active_trace(trace)
+
+        values = self.get_trace_values(trace)
+        trac_data = {}
+        for i in range(len(self._item_names)):
+            trac_data[self._item_names[i]] = (values[i], self._units[i])
+        return trac_data
+
+    def get_trace_params(self, trace, param_names):
+        trac_data = self.get_formatted_trace_data(trace)
+        return [trac_data[i] for i in param_names]
---
+++ models/M8292A.py
@@ -0,0 +1,13 @@
+from ..base_models.VsaOSA import ModelVsaOSA
+
+class ModelM8292A(ModelVsaOSA):
+    model = "M8292A"
+    brand = "Keysight"
+    details = {
+        "Maximum detectable symbol rate": "74 GHz",
+        "Wavelength range": "1527.60 to 1570.01 nm (196.25 to 190.95 THz)",
+        }
+
+    def __init__(self, resource_name, **kwargs):
+        super(ModelM8292A, self).__init__(resource_name, **kwargs)
+        
